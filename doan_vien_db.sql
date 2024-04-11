d9aee06157bff0d128675613200a9909da4c009e
Thien <admin@admins-MacBook-Pro.local>
2024-04-11 08:19:37 +0700

    file sql

---
+++ doan_vien_db.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100427 (10.4.27-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : doan_vien_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 100427 (10.4.27-MariaDB)
+ File Encoding         : 65001
+
+ Date: 11/04/2024 08:18:21
+*/
+
